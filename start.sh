f0508bc692b1e1fc2291c7c426c3e34d8bded7c5
GRoot <114850652+XproDecoy@users.noreply.github.com>
2022-12-09 22:42:40 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/XproDecoy/Advance-EvaMaria.git /EvaMaria
+  git clone https://github.com/XproDecoy/Tomfilter.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
